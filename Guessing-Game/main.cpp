781fd54019e24962e71206379f55d07c15121bcf
Yvan Gauthier <y.gauthier04@icloud.com>
2024-09-11 07:32:37 +0200

    user input

--- Guessing-Game/main.cpp
+++ Guessing-Game/main.cpp
@@ -6,9 +6,58 @@
 //
 
 #include <iostream>
+using namespace std;
+
+// Function declaration
+void user_input() {
+    
+    char rep;
+    
+    // Ask if the user want to play or no
+    cout<<"#################################";
+    cout<<" GUESSING GAME ";
+    cout<<"#################################\n";
+    cout<<"\n";
+    cout<<"Hello, would you like to play a game y or n : ";
+    cin>> rep;
+    if (rep == 'y')
+    {
+        // Test
+        cout<<"Yes\n";
+    }
+    else if (rep == 'Y')
+    {
+        // Test
+        cout<<"Yes\n";
+        
+    }else if (rep == 'n')
+    {
+        cout<<"Ok may be later bye !\n";
+    }else if (rep == 'N')
+    {
+        cout<<"Ok may be later bye !\n";
+    }
+
+}
+/// Level hard
+void hard_level() {
+    //Code
+    
+}
+/// Level medium
+void meduim_level() {
+    //Code
+}
+/// Level easy
+void easy_level(){
+    // Code
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Test
+    user_input();
     return 0;
+   
 }
+// This is a program written in C++.
+// This is a game that you will play in a console. 
